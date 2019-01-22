467b8c94c2fb232a1c374861c5b3eb1dbb5f99ab
Rasmus Bengtsson <git@rasmusbengtsson.se>
2019-01-22 21:25:18 +0100

    Probably help minimize build errors

--- test/3-file-write-test.js
+++ test/3-file-write-test.js
@@ -8,26 +8,36 @@ const fs = require('fs');
 const os = require('os');
 
 describe('File write tests', () => {
-  it('should write a file named translations.pot when no destination is given', () => {
+  it('should write a file named translations.pot when no destination is given', (done) => {
     const currentDir = process.cwd();
     process.chdir(os.tmpdir()); // Set temporary directory for file creation
-    fs.unlink('translations.pot', () => {}); // Remove file if existing
-    wpPot({
-      src: 'test/fixures/empty-dir/*.php'
+
+    // Remove file if existing
+    fs.unlink('translations.pot', () => {
+      wpPot({
+        src: 'test/fixures/empty-dir/*.php'
+      });
+
+      assert(fs.statSync('translations.pot').isFile());
+
+      fs.unlinkSync('translations.pot'); // Remove file
+      process.chdir(currentDir); // Set temporary directory for file creation
+      done();
     });
-    assert(fs.statSync('translations.pot').isFile());
-    fs.unlink('translations.pot', () => {}); // Remove file
-    process.chdir(currentDir); // Set temporary directory for file creation
   });
 
-  it('should write a file to the correct destination when given', () => {
+  it('should write a file to the correct destination when given', (done) => {
     const tempPot = os.tmpdir() + '/file-write-test.pot'; // Set temporary directory for file creation
-    fs.unlink(tempPot, () => {}); // Remove file if existing
-    wpPot({
-      src: 'test/fixures/empty-dir/*.php',
-      destFile: tempPot
+
+    // Remove file if existing
+    fs.unlink(tempPot, () => {
+      wpPot({
+        src: 'test/fixures/empty-dir/*.php',
+        destFile: tempPot
+      });
+      assert(fs.statSync(tempPot).isFile());
+      fs.unlinkSync(tempPot); // Remove file
+      done();
     });
-    assert(fs.statSync(tempPot).isFile());
-    fs.unlink(tempPot, () => {}); // Remove file
   });
 });
