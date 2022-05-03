417b8ecf981c59ff28056d6408e97ed8948b7e7c
Philip <philipvanheemstra@hotmail.com>
2022-05-03 16:40:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -158,14 +158,14 @@ function setHeaders (options) {
   }
 
   if (!options.noCreationDate) {
-    let d = new Date();
-    let nowString = [
+    const d = new Date();
+    const nowString = [
       `${d.getUTCFullYear()}`,
-      `-${String(d.getUTCMonth()+1).padStart(2, '0')}`,
+      `-${String(d.getUTCMonth() + 1).padStart(2, '0')}`,
       `-${String(d.getUTCDate()).padStart(2, '0')}`,
       ` ${String(d.getUTCHours()).padStart(2, '0')}`,
       `:${String(d.getUTCMinutes()).padStart(2, '0')}`,
-      `+0000`
+      '+0000'
     ].join('');
     options.headers['POT-Creation-Date'] = nowString;
   }
